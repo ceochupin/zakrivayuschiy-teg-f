e3db7d674b02caa677772b7188845ca08ec31c49
Vladimir Chupin <ceochupin@yandex.ru>
2024-10-08 03:05:36 +0300

    fix upload favicon in root /dist/

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const autoprefixer = require('autoprefixer');
-const glob = require('glob');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
@@ -17,81 +16,60 @@ module.exports = {
       {
         test: /\.js$/i,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
+        use: 'babel-loader',
       },
       {
         test: /\.s[ac]ss$/i,
         use: [
           MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
-            },
-          },
+          { loader: 'css-loader', options: { sourceMap: true } },
           {
             loader: 'postcss-loader',
             options: {
               sourceMap: true,
-              postcssOptions: {
-                plugins: [
-                  autoprefixer()
-                ],
-              },
+              postcssOptions: { plugins: [autoprefixer()] },
             },
           },
-          {
-            loader: 'sass-loader',
-          },
+          'sass-loader',
         ],
       },
+      { test: /\.html$/i, use: 'html-loader' },
       {
-        test: /\.html$/i,
-        use: ['html-loader']
+        test: /\.(png|jpe?g|gif|aif)$/i,
+        type: 'asset/resource',
+        generator: { filename: 'images/[name].[hash][ext]' },
+        include: [path.resolve(__dirname, 'src/images')],
       },
       {
-        test: /\.(png|jpg|jpeg|gif|aif|ico)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'images/[name].[hash][ext]',
-        },
-        include: [
-          path.resolve(__dirname, 'src/images')
+        test: /\.(svg|ico)$/i,
+        oneOf: [
+          {
+            test: /favicon\.(svg|ico)$/i,
+            type: 'asset/resource',
+            generator: { filename: '[name][ext]' },
+          },
+          {
+            type: 'asset/resource',
+            generator: { filename: 'svg/[name].[hash][ext]' },
+            include: [path.resolve(__dirname, 'src/svg')],
+          }
         ]
       },
       {
-        test: /\.svg$/i,
+        test: /\.(woff2?|eot|ttf|otf)$/i,
         type: 'asset/resource',
-        generator: {
-          filename: 'svg/[name].[hash][ext]',
-        },
-        include: [
-          path.resolve(__dirname, 'src/svg')
-        ]
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'fonts/[name].[hash][ext]',
-        },
+        generator: { filename: 'fonts/[name].[hash][ext]' },
       },
     ],
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
     new MiniCssExtractPlugin({
       filename: 'styles/[name].[contenthash].css',
       chunkFilename: 'styles/[id].[contenthash].css',
     }),
   ],
   resolve: {
-    fallback: {
-      util: require.resolve("util/")
-    }
-  }
+    fallback: { util: require.resolve("util/") },
+  },
 };
